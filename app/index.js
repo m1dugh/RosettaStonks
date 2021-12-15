6545c993f6a25f9ba75c7c08918d1a1845368e57
m1dugh <romain103paris@gmail.com>
2021-12-15 12:19:50 +0100

    adds style

--- app/index.js
+++ app/index.js
@@ -2,7 +2,8 @@ const status = document.getElementById("status")
 const timeCounter = document.getElementById("timeCounter");
 
 const [form] = document.getElementsByTagName("form");
-const timeInput = form.firstElementChild;
+const timeInput = form.getElementsByTagName("input")[0];
+const submitButton = form.getElementsByTagName("button")[0];
 
 
 let appReady = false;
@@ -25,7 +26,9 @@ function print_data(data) {
 
 chrome.storage.onChanged.addListener(UpdateStatusPanel)
 window.onload = async () => {
+    print_data("pre update")
     UpdateStatusPanel();
+    print_data("post update")
     timeInput.addEventListener("change", (e) => {
         e.preventDefault()
         const value = parseInt(e.target.value)
@@ -46,36 +49,45 @@ window.onload = async () => {
         if (minutes <= 0 || isNaN(minutes))
             return;
 
-        const maxRequestCount = Math.floor(minutes / 20);
-        const lastRequestMinutes = minutes % 20;
-        for (let i = 0; i < maxRequestCount; i++)
-            SendRequest();
-        if (lastRequestMinutes !== 0)
-            SendRequest(lastRequestMinutes);
+        SendRequest(minutes)
+
     };
 }
 
-function SendRequest(minutes = 20) {
-    chrome.storage.sync.get(["request"], async ({request}) => {
+function SendRequest(minutes) {
+    chrome.storage.sync.get(["request"], ({request}) => {
         const body = new DOMParser().parseFromString(request.body, "text/xml");
-        const time = minutes * 60 * 1000;
-        body.documentElement.getElementsByTagName("delta_time")[0].innerHTML = time.toString()
-        body.documentElement.getElementsByTagName("updated_at")[0].innerHTML = Date.now().toString()
+        const maxTime = 20 * 60 * 1000;
 
         const rootTag = body.documentElement.tagName
-        const data = `<${rootTag}>${body.documentElement.innerHTML}</${rootTag}>`
 
 
-        request.headers["Access-Control-Allow-Origin"]= "*"
+        request.headers["Access-Control-Allow-Origin"] = "*"
 
-        fetch(request.url, {
-            method: "POST",
-            headers: request.headers,
-            body: data
-        }).then((res) => print_data(res.status))
-            .catch((err) => print_data(err.stack))
+        const promises = []
+        while (minutes > 0) {
+            body.documentElement.getElementsByTagName("delta_time")[0].innerHTML =
+                (minutes > 20 ? 20 : minutes).toString()
+            body.documentElement.getElementsByTagName("updated_at")[0].innerHTML =
+                Date.now().toString()
+            const data = `<${rootTag}>${body.documentElement.innerHTML}</${rootTag}>`
+            promises.push(fetch(request.url, {
+                method: "POST",
+                headers: request.headers,
+                body: data
+            }).then((res) => print_data(res.status))
+                .catch((err) => print_data(err.stack)))
 
-        print_data({url: request.url, data, headers: request.headers})
+            minutes -= 20;
+
+        }
+
+        const text = submitButton.innerText
+        submitButton.innerText = "...";
+        Promise.all(promises).then(() => {
+            submitButton.innerText = text
+        })
+
 
     })
 }
@@ -93,10 +105,10 @@ function UpdateStatusPanel() {
                 minimumIntegerDigits: 2,
                 maximumFractionDigits: 0
             });
-            const offset = (Date.now() - request.timestamp) / 1000
+            const offset = Math.floor((Date.now() - request.timestamp) / 1000)
             const [hours, minutes] = [
-                format.format(offset / 3600),
-                format.format((offset / 60) % 60)
+                format.format(Math.floor(offset / 3600)),
+                format.format(Math.floor(offset / 60) % 60)
             ]
             timeCounter.innerText = `${hours}h${minutes}m`
         }
