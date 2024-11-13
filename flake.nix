6ee3003352b200bb0e8c5d5c28cd65166bfaab16
m1dugh <romain.le-miere@epita.fr>
2024-11-13 15:45:22 +0100

    feat(refacto): re-inits project

---
+++ flake.nix
@@ -0,0 +1,35 @@
+# vi: tabstop=2 shiftwidth=2
+{
+  description = "A development flake for rosetta stonks project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            deno
+          ];
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
