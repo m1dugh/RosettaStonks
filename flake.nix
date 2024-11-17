cdce9c5859d165987255baa1b43faf4b3dbbfa64
m1dugh <romain.le-miere@epita.fr>
2024-11-18 00:19:05 +0100

    feat(flake): adds packages for chrome and firefox versions

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        inherit (nixpkgs) lib;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -30,6 +31,94 @@
             zip
           ];
         };
+
+        packages = 
+        let
+          buildDenoPackage = {
+            denoHash,
+            pname,
+            src,
+            ...
+          }@drv:
+          let
+            deno-cache = builtins.derivation {
+              name = "deno-cache";
+              src = ./.;
+              inherit system;
+              builder = 
+              let
+                builder = pkgs.writeShellScriptBin "builder" ''
+                ${pkgs.coreutils}/bin/mkdir -p $out/cache/
+                echo $src
+                bite
+                cd $src
+                DENO_DIR="$out/cache/" ${lib.getExe pkgs.deno} install
+                '';
+              in "${lib.getExe builder}";
+
+              outputHashMode = "recursive";
+              outputHashAlgo = "sha256";
+              outputHash = denoHash;
+            };
+          in pkgs.stdenv.mkDerivation ({
+            inherit src;
+            name = pname;
+
+            preBuild = ''
+              mkdir -p $out/
+              cp -R ${deno-cache}/cache/ $out/cache
+              chmod -R +w $out/cache/
+              export DENO_DIR="$out/cache/"
+            '';
+
+            postBuild = ''
+              rm -rf $out/cache/
+            '';
+          } // drv);
+        in {
+          mozilla = buildDenoPackage {
+            pname = "rosettastonks.xpi";
+            denoHash = "sha256-/aMoLC9ZlYTZI1FMvQvelHQP+GMhG3f2RjpZQ6tStiw=";
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+              deno
+              gnumake
+              zip
+            ];
+
+            buildTargets = ["mozilla"];
+
+            installPhase = ''
+              runHook preInstall
+              cp rosettastonks.xpi $out
+              runHook postInstall
+            '';
+
+          };
+          chrome = buildDenoPackage {
+
+            pname = "rosettastonks-chrome";
+            denoHash = "sha256-/aMoLC9ZlYTZI1FMvQvelHQP+GMhG3f2RjpZQ6tStiw=";
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+              deno
+              gnumake
+              zip
+            ];
+
+            buildTargets = ["chrome"];
+
+            installPhase = ''
+              runHook preInstall
+              cp -R dist/ static/ manifest.json $out/
+              runHook postInstall
+            '';
+
+          };
+        };
+
 
         formatter = pkgs.nixfmt-rfc-style;
       }
